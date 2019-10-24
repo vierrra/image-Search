d769d4ed57b30f9374cd465d9c0a24fc19db0099
Stant <stant@Mini-de-Stant-2.stant.local>
2019-10-24 13:26:58 -0300

    Added offline internet alert

--- image-search/ViewControllers/ViewController.swift
+++ image-search/ViewControllers/ViewController.swift
@@ -19,66 +19,67 @@ class ViewController: UIViewController {
     @IBOutlet weak var labelNameArtist : UILabel!
     @IBOutlet weak var button          : UIButton!
     
-override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
         button.layer.cornerRadius = 27
+    }
     
-}
-    
-     @IBAction func alterText(_ sender: Any) {
+        @IBAction func alterText(_ sender: Any) {
         
-            activityIndicator.center                     = self.view.center
-            activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorView.Style.white
-            view.addSubview(activityIndicator)
-            self.activityIndicator.startAnimating()
-            
-            guard let fakeUrl = URL(string: "http://fakerapiexample.herokuapp.com/album") else { return }
-            
-            URLSession.shared.dataTask(with: fakeUrl) { (data, response, error) in
-               
-                                             
-                guard let data = data else {return}
-               
-                     do {
-                         let decoder = JSONDecoder()
-                         let gitData = try decoder.decode(SearchImages.self, from: data)
-                        
-                            DispatchQueue.main.sync {
+                activityIndicator.center                     = self.view.center
+                activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorView.Style.white
+                view.addSubview(activityIndicator)
+                self.activityIndicator.startAnimating()
+                
+                guard let fakeUrl = URL(string: "http://fakerapiexample.herokuapp.com/album") else { return }
+                
+                URLSession.shared.dataTask(with: fakeUrl) { (data, response, error) in
                                                                 
-                                    if let url = URL(string: gitData.albumCover ?? "") {
-                                       let data = try? Data(contentsOf: url)
-                                       let image: UIImage     = UIImage(data: data!)!
-                                       let imageBlur: UIImage = UIImage(data: data!)!
-                                       self.imageBlur.image   = imageBlur
-                                       self.image.image       = image
-                                      
-                                    }
-
-                                    if let anameAlbum            = gitData.albumName {
-                                        self.labelNameAlbum.text = anameAlbum
-                                    }
-                                    
-                                    if let anameArtist            = gitData.artistName {
-                                        self.labelNameArtist.text = anameArtist
-                                    }
-                                    
-                                    if let anameAlbumCover        = gitData.recordLabel {
-                                        self.labelAlbumCover.text = anameAlbumCover
-                                    }
-                                    
-                                self.activityIndicator.stopAnimating()
-                        }
-
-                    } catch {
-                         
-                        DispatchQueue.main.async {
-                            Alert().display(self, "Attention", error.localizedDescription)
+                    guard let data = data else {
+                        
+                       DispatchQueue.main.async {
+                            Alert().display(self, "Attention", error?.localizedDescription ?? "Internet offline")
                             self.activityIndicator.stopAnimating()
                         }
                         
-                    }
-            }.resume()
-            
-    }
-    
+                    return}
+                   
+                         do {
+                             let decoder = JSONDecoder()
+                             let gitData = try decoder.decode(SearchImages.self, from: data)
+                            
+                                DispatchQueue.main.sync {
+                                                                    
+                                        if let url = URL(string: gitData.albumCover ?? "") {
+                                           let data = try? Data(contentsOf: url)
+                                           let image: UIImage     = UIImage(data: data!)!
+                                           let imageBlur: UIImage = UIImage(data: data!)!
+                                           self.imageBlur.image   = imageBlur
+                                           self.image.image       = image
+                                        }
+
+                                        if let anameAlbum             = gitData.albumName {
+                                            self.labelNameAlbum.text  = anameAlbum
+                                        }
+                                        
+                                        if let anameArtist            = gitData.artistName {
+                                            self.labelNameArtist.text = anameArtist
+                                        }
+                                        
+                                        if let anameAlbumCover        = gitData.recordLabel {
+                                            self.labelAlbumCover.text = anameAlbumCover
+                                        }
+                                        
+                                    self.activityIndicator.stopAnimating()
+                               }
+
+                        } catch {
+                             
+                            DispatchQueue.main.async {
+                                Alert().display(self, "Attention", error.localizedDescription)
+                                self.activityIndicator.stopAnimating()
+                            }
+                        }
+                }.resume()
+      }
 }
